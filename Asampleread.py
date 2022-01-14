8b0f20613ff1a37cece9ae9ae6b82439981307b0
LeoZ06 <79598689+LeoZ06@users.noreply.github.com>
2022-01-14 17:17:12 +0000

    A

--- Asampleread.py
+++ Asampleread.py
@@ -1,3 +1,7 @@
 """
 Write a program (doesn't have to be a function, but can be) that reads what's in sample.txt and prints it to the console.
 """
+fstream = open('sample.txt')
+content = fstream.read()
+print(content)
+fstream.close()
